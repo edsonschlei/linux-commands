9fa725af13ef42e65c1d7a1e3d99d42089204854
Unknown <unknown@example.com>
2017-10-21 17:10:00 +0200

    sdkman
    - sdkman install the basic programs used with java development

---
+++ sdkman.sh
@@ -0,0 +1,20 @@
+
+curl -s "https://get.sdkman.io" | bash
+
+source ~/.sdkman/bin/sdkman-init.sh
+
+## java
+sdk i java
+sdk i java 8u151-oracle
+
+## gradle
+sdk i gradle
+
+## Springboot
+sdk i springboot
+
+## Maven
+sdk i maven
+
+## Groovy
+sdk i groovy
