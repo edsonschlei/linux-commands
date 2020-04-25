94e141232e2a4560a276ea2aa8f46418da4b9f93
Edson Schlei <edson.schlei@gmail.com>
2020-04-25 18:42:57 +0200

    ubuntu 2ß.04
    - update script and configuration to ubuntu 20.04

--- install.sh
+++ install.sh
@@ -6,50 +6,53 @@
 #
 
 # install curl
-sudo apt install curl
+sudo apt install -y curl
 
 # Removing install snap
-# sudo apt purge snapd ubuntu-core-launcher squashfs-tools
-# sudo apt install gnome-system-monitor gnome-characters gnome-logs gnome-calculator
+sudo apt purge -y snapd ubuntu-core-launcher squashfs-tools
+sudo apt install gnome-system-monitor gnome-characters gnome-logs gnome-calculator
 
 # install gdebi
-sudo apt-get install -y gdebi
+sudo apt install -y gdebi
 
 # install chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+sudo apt update
+sudo apt install -y google-chrome-stable
 
 # install flameshot
 sudo apt install -y flameshot
 
 # install keepassxc
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt update && sudo apt install -y keepassxc
+# sudo add-apt-repository ppa:phoerious/keepassxc
+# sudo apt update && 
+sudo apt install -y keepassxc
 
 # install atom
 # sudo add-apt-repository ppa:webupd8team/atom
-# sudo apt-get update && sudo apt-get install -y atom
+# sudo apt update && sudo apt install -y atom
 
 # install brackets
 # sudo add-apt-repository ppa:webupd8team/brackets
 # sudo apt update && sudo apt install -y brackets
 
 # spotify dependencies
-sudo apt-get install libcanberra-gtk-module
+sudo apt install libcanberra-gtk-module
 
 # install spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install -y spotify-client
 
 ## extra video and audio codes
 
+# libdvd-pkg libdvdnav4
+# sudo dpkg-reconfigure libdvd-pkg
+
 sudo apt update
-sudo apt install libdvdnav4 libdvdread4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
-sudo dpkg-reconfigure libdvd-pkg
+sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly 
 sudo apt install ubuntu-restricted-extras
 
 
@@ -58,14 +61,17 @@ sudo apt install ubuntu-restricted-extras
 # sudo apt install -y libreoffice-help-de
 
 # install vlc
-sudo install install -y vlc
+sudo apt install -y vlc
+
+# to fix the libdvdcss error from vlc install
+sudo dpkg-reconfigure libdvd-pkg
 
 # install git
 sudo apt install -y git
 
 # install gitkraken
 cd /tmp
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && sudo gdebi gitkraken-amd64.deb
+wget https://release.axocdn.com/linux/gitkraken-amd64.deb && sudo gdebi gitkraken-amd64.deb
 
 sudo apt install -y gnome-tweak-tool
 
@@ -84,52 +90,59 @@ sudo tlp start
 # no more java from oracle, use openjdk
 #sudo add-apt-repository ppa:webupd8team/java
 #echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-#sudo apt-get update && sudo apt-get install -y oracle-java8-installer
+#sudo apt update && sudo apt install -y oracle-java8-installer
 #sudo apt install -y oracle-java8-set-default
 
+sudo apt install -y default-jdk
+
 # fonts inconsolata
-sudo apt-get install -y fonts-inconsolata
+sudo apt install -y fonts-inconsolata
 
 # Google clound tools
 
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update 
-sudo apt-get install -y google-cloud-sdk
-sudo apt-get install -y google-cloud-sdk-app-engine-java
-sudo apt-get install -y google-cloud-sdk-app-engine-python
-sudo apt-get install -y google-cloud-sdk-app-engine-python-extras
-sudo apt-get install -y google-cloud-sdk-app-engine-java
-sudo apt-get install -y google-cloud-sdk-app-engine-go
-sudo apt-get install -y google-cloud-sdk-datalab
-sudo apt-get install -y google-cloud-sdk-datastore-emulator
-sudo apt-get install -y google-cloud-sdk-pubsub-emulator
-sudo apt-get install -y google-cloud-sdk-cbt
-sudo apt-get install -y google-cloud-sdk-cloud-build-local
-sudo apt-get install -y google-cloud-sdk-bigtable-emulator
-sudo apt-get install -y kubectl
+sudo apt update 
+sudo apt install -y google-cloud-sdk
+sudo apt install -y google-cloud-sdk-app-engine-java
+sudo apt install -y google-cloud-sdk-app-engine-python
+sudo apt install -y google-cloud-sdk-app-engine-python-extras
+sudo apt install -y google-cloud-sdk-app-engine-go
+sudo apt install -y google-cloud-sdk-datalab
+sudo apt install -y google-cloud-sdk-datastore-emulator
+sudo apt install -y google-cloud-sdk-pubsub-emulator
+sudo apt install -y google-cloud-sdk-cbt
+sudo apt install -y google-cloud-sdk-cloud-build-local
+sudo apt install -y google-cloud-sdk-bigtable-emulator
+sudo apt install -y kubectl
+
 
 # Visual Studio code
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install -y code
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install -y code
 
-# Diffuse
-
-sudo apt-get install -y diffuse
+# Vim and diff tools
+# gvim, vim, gvimdiff
+sudo apt install vim vim-gtk3
 
 # SAMBA Tools
-sudo apt-get install -y cifs-utils keyutils 
+sudo apt install -y cifs-utils keyutils 
+
+# ssh fs - to mount remout ssh
+sudo apt install sshfs
 
 ## Vivaldi
 
-wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
+# wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
 sudo apt install -y vivaldi-stable
 
 
+## Android Tools
+sudo apt install android-tools-adb android-tools-fastboot
--- ubuntu.md
+++ ubuntu.md
@@ -87,8 +87,14 @@ Page: [How to install Google Chrome](https://askubuntu.com/questions/510056/how-
 ```
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt update
+sudo apt install google-chrome-stable
+sudo apt install chrome-gnome-shell
+```
+
+### Gnome shell for chrome
+
+```
 sudo apt-get install chrome-gnome-shell
 ```
 
@@ -120,15 +126,14 @@ Download link (Opera version 48)[http://download1.operacdn.com/pub/opera/desktop
 ## KeepassXC
 
 ```
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt update && sudo apt install keepassxc
+sudo apt install keepassxc
 ```
 
 ## Atom
 
 ```
 sudo add-apt-repository ppa:webupd8team/atom
-sudo apt-get update && sudo apt-get install atom
+sudo apt update && sudo apt install atom
 ```
 
 ## Brackets
@@ -145,8 +150,8 @@ page: [Spotify for Linux](https://www.spotify.com/lu-fr/download/linux/)
 ```
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt update
+sudo apt install spotify-client
 ```
 
 ## extra video and audio codes
@@ -172,7 +177,7 @@ sudo apt install libreoffice
 ### using a RC versions
 ```
 sudo apt remove --purge libreoffice*
-sudo apt-get install python-software-properties
+sudo apt install python-software-properties
 sudo apt-add-repository ppa:libreoffice/ppa
 sudo apt update
 sudo apt install -y libreoffice
@@ -292,7 +297,7 @@ sudo apt install ubuntu-restricted-extras
 ## Calibre
 
 ```
-sudo apt-get install calibre
+sudo apt install calibre
 ```
 
 ## Prolong your battery and prevent overheating
@@ -310,32 +315,33 @@ sudo tlp start
 ## fonts inconsolata
 
 ```
-sudo apt-get install fonts-inconsolata
+sudo apt install fonts-inconsolata
 ```
 
 ## Google clound tools
 
-[Installing with apt-get (Debian and Ubuntu only)](https://cloud.google.com/sdk/docs/downloads-apt-get)
+[Installing with apt (Debian and Ubuntu only)](https://cloud.google.com/sdk/docs/downloads-apt-get)
 
 ```
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update 
-sudo apt-get install google-cloud-sdk
-sudo apt-get install google-cloud-sdk-app-engine-java
-sudo apt-get install google-cloud-sdk-app-engine-python
-sudo apt-get install google-cloud-sdk-app-engine-python-extras
-sudo apt-get install google-cloud-sdk-app-engine-java
-sudo apt-get install google-cloud-sdk-app-engine-go
-sudo apt-get install google-cloud-sdk-datalab
-sudo apt-get install google-cloud-sdk-datastore-emulator
-sudo apt-get install google-cloud-sdk-pubsub-emulator
-sudo apt-get install google-cloud-sdk-cbt
-sudo apt-get install google-cloud-sdk-cloud-build-local
-sudo apt-get install google-cloud-sdk-bigtable-emulator
-sudo apt-get install kubectl
+sudo apt update 
+sudo apt install -y --install-suggests --install-recommends google-cloud-sdk
+sudo apt install -y google-cloud-sdk-app-engine-java
+sudo apt install -y google-cloud-sdk-app-engine-python
+sudo apt install -y google-cloud-sdk-app-engine-python-extras
+sudo apt install -y google-cloud-sdk-app-engine-go
+sudo apt install -y google-cloud-sdk-datalab
+sudo apt install -y google-cloud-sdk-datastore-emulator
+sudo apt install -y google-cloud-sdk-pubsub-emulator
+sudo apt install -y google-cloud-sdk-cbt
+sudo apt install -y google-cloud-sdk-cloud-build-local
+sudo apt install -y google-cloud-sdk-bigtable-emulator
+sudo apt install -y kubectl
 ```
+
+
 
 ## Visual Studio code
 
@@ -343,22 +349,23 @@ sudo apt-get install kubectl
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install -y code
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install -y code
 
 ```
 
-## Diffuse
+## Vim and Diff tools
 
 ```
-sudo apt-get install diffuse
+sudo apt install vim vim-gtk3
+
 ```
 
 ## SAMBA Tools
 
 ```
-sudo apt-get install cifs-utils keyutils 
+sudo apt install cifs-utils keyutils 
 ```
 
 ## Signal
@@ -376,5 +383,5 @@ The Tools to install the new lineage version on your android phone.
 [Android Tools](http://www.bernaerts-nicolas.fr/linux/74-ubuntu/354-ubuntu-xenial-android-adb-fastboot-qtadb)
 
 ```
-sudo apt-get install android-tools-adb android-tools-fastboot
+sudo apt install android-tools-adb android-tools-fastboot
 ```
